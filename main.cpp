0234410253b0ec57b64b9263d4f02b40c0765477
curiosus <jepeterson@gmail.com>
2015-05-24 14:27:03 -0600

    set up game loop and other boilerplate

--- main.cpp
+++ main.cpp
@@ -1,9 +1,40 @@
-#include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <vector>
 
-using std::cout;
-using std::endl;
+/*
+http://curiosus.me
+jepeterson@gmail.com
+2015-05-24-14:04MDT
+*/
+
+using std::vector;
+using sf::RenderWindow;
+using sf::CircleShape;
+using sf::RectangleShape;
+using sf::Color;
+using sf::Event;
+using sf::Keyboard;
+
+constexpr int fps{60};
+constexpr int windowWidth{1200};
+constexpr int windowHeight{900};
 
 int main() {
-  cout << "Hello Whirled Peas!" << endl;
+  RenderWindow window{{windowWidth, windowHeight}, "game0"};
+  window.setFramerateLimit(fps);
+  while (window.isOpen()) {
+    window.clear(Color::Black);
+    Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == Event::Closed) {
+        window.close();
+        break;
+      }
+    }
+
+    window.display();
+
+  }
   return 0;
 }
