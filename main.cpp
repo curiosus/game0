179331f70213e88d13df9c3b5c26573560b0b307
curiosus <jepeterson@gmail.com>
2015-05-24 16:21:41 -0600

    Set up Striker class

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <vector>
@@ -9,20 +10,53 @@ jepeterson@gmail.com
 */
 
 using std::vector;
+using std::cout;
+using std::endl;
+
 using sf::RenderWindow;
 using sf::CircleShape;
 using sf::RectangleShape;
 using sf::Color;
 using sf::Event;
 using sf::Keyboard;
+using sf::Vector2f;
 
 constexpr int fps{60};
 constexpr int windowWidth{1200};
 constexpr int windowHeight{900};
 
+
+//Striker Class
+//Player uses striker to strike other objects 
+//TODO Turn this into a class when I learn classes in my uhhh...class. Actually it's a book or two.
+constexpr float strikerWidth{60.f}; 
+constexpr float strikerHeight{20.f};
+struct Striker {
+
+  RectangleShape shape;
+  Vector2f coordinates;
+
+  Striker () {
+    shape.setPosition(10.f, 10.f);
+    shape.setSize({strikerWidth, strikerHeight});
+    shape.setFillColor(Color::Blue);
+    shape.setOrigin(strikerWidth / 2.f, strikerHeight / 2.f);
+  }
+
+  void update() {
+    
+  }
+  
+  
+};
+
 int main() {
+
   RenderWindow window{{windowWidth, windowHeight}, "game0"};
   window.setFramerateLimit(fps);
+
+  Striker striker;
+
   while (window.isOpen()) {
     window.clear(Color::Black);
     Event event;
@@ -32,7 +66,7 @@ int main() {
         break;
       }
     }
-
+    
     window.display();
 
   }
